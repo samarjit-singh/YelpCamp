9f4a015d429f25bab54e8906def705377e0451c7
samarjit-singh <75423063+samarjit-singh@users.noreply.github.com>
2022-01-22 10:22:03 +0530

    associated user with campground they made

--- seeds/index.js
+++ seeds/index.js
@@ -21,6 +21,7 @@ const seedDB = async () => {
     const random1000 = Math.floor(Math.random() * 210);
     const price = Math.floor(Math.random() * 20) + 10;
     const camp = new Campground({
+      author: "61ea9688c53326b013bded5f",
       location: `${cities[random1000].city}, ${cities[random1000].state}`,
       title: `${sample(descriptors)} ${sample(places)}`,
       image: "https://source.unsplash.com/random/483251",
