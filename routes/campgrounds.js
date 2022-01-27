dc9f11d29be697881d00ca771bb8c9d593c667a4
samarjit-singh <75423063+samarjit-singh@users.noreply.github.com>
2022-01-27 14:51:23 +0530

    Adding Authorization to the campgrounds

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -1,23 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const catchAsync = require("../utils/catchAsync");
-const { campgroundSchema } = require("../schemas.js");
-const { isLoggedIn } = require("../middleware");
-
-const ExpressError = require("../utils/ExpressError");
 const Campground = require("../models/campground");
-const flash = require("connect-flash/lib/flash");
 
-// validation on the server side
-const validateCampground = (req, res, next) => {
-  const { error } = campgroundSchema.validate(req.body);
-  if (error) {
-    const msg = error.details.map((el) => el.message).join(",");
-    throw new ExpressError(msg, 400);
-  } else {
-    next();
-  }
-};
+const { isLoggedIn, isAuthor, validateCampground } = require("../middleware");
+
+const flash = require("connect-flash/lib/flash");
 
 router.get(
   "/",
@@ -62,8 +50,10 @@ router.get(
 router.get(
   "/:id/edit",
   isLoggedIn,
+  isAuthor,
   catchAsync(async (req, res) => {
-    const campground = await Campground.findById(req.params.id);
+    const { id } = req.params;
+    const campground = await Campground.findById(id);
     if (!campground) {
       req.flash("error", "Cannot find that campground!");
       return res.redirect("/campgrounds");
@@ -75,6 +65,7 @@ router.get(
 router.put(
   "/:id",
   isLoggedIn,
+  isAuthor,
   validateCampground,
   catchAsync(async (req, res) => {
     const { id } = req.params;
@@ -89,6 +80,7 @@ router.put(
 router.delete(
   "/:id",
   isLoggedIn,
+  isAuthor,
   catchAsync(async (req, res) => {
     const { id } = req.params;
     await Campground.findByIdAndDelete(id);
