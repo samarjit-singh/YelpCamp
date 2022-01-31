2b855ca249d2b2bd79857c3684c11332c9cf9a8f
samarjit-singh <75423063+samarjit-singh@users.noreply.github.com>
2022-01-31 13:36:16 +0530

    uploading images on cloudinary and adding environment variables

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -3,6 +3,9 @@ const router = express.Router();
 const campgrounds = require("../controllers/campgrounds");
 const catchAsync = require("../utils/catchAsync");
 const Campground = require("../models/campground");
+const multer = require("multer");
+const { storage } = require("../cloudinary");
+const upload = multer({ storage });
 
 const { isLoggedIn, isAuthor, validateCampground } = require("../middleware");
 
@@ -11,11 +14,15 @@ const flash = require("connect-flash/lib/flash");
 router
   .route("/")
   .get(catchAsync(campgrounds.index))
-  .post(
-    isLoggedIn,
-    validateCampground,
-    catchAsync(campgrounds.createCampground)
-  );
+  // .post(
+  //   isLoggedIn,
+  //   validateCampground,
+  //   catchAsync(campgrounds.createCampground)
+  // );
+  .post(upload.array("image"), (req, res) => {
+    console.log(req.body, req.files);
+    res.send("it worked");
+  });
 
 router.get("/new", isLoggedIn, campgrounds.renderNewForm);
 
