34b58259f9c20f0795851d4647ef3e4d8c0db038
samarjit-singh <75423063+samarjit-singh@users.noreply.github.com>
2022-01-31 18:21:39 +0530

    adding and deleting images part 2

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -2,27 +2,22 @@ const express = require("express");
 const router = express.Router();
 const campgrounds = require("../controllers/campgrounds");
 const catchAsync = require("../utils/catchAsync");
-const Campground = require("../models/campground");
+const { isLoggedIn, isAuthor, validateCampground } = require("../middleware");
 const multer = require("multer");
 const { storage } = require("../cloudinary");
 const upload = multer({ storage });
 
-const { isLoggedIn, isAuthor, validateCampground } = require("../middleware");
-
-const flash = require("connect-flash/lib/flash");
+const Campground = require("../models/campground");
 
 router
   .route("/")
   .get(catchAsync(campgrounds.index))
-  // .post(
-  //   isLoggedIn,
-  //   validateCampground,
-  //   catchAsync(campgrounds.createCampground)
-  // );
-  .post(upload.array("image"), (req, res) => {
-    console.log(req.body, req.files);
-    res.send("it worked");
-  });
+  .post(
+    isLoggedIn,
+    upload.array("image"),
+    validateCampground,
+    catchAsync(campgrounds.createCampground)
+  );
 
 router.get("/new", isLoggedIn, campgrounds.renderNewForm);
 
@@ -32,6 +27,7 @@ router
   .put(
     isLoggedIn,
     isAuthor,
+    upload.array("image"),
     validateCampground,
     catchAsync(campgrounds.updateCampground)
   )
--- seeds/index.js
+++ seeds/index.js
@@ -3,31 +3,43 @@ const cities = require("./cities");
 const { places, descriptors } = require("./seedHelpers");
 const Campground = require("../models/campground");
 
-mongoose
-  .connect("mongodb://localhost:27017/yelp-camp")
-  .then(() => {
-    console.log("MONGO CONNECTION OPEN!!!");
-  })
-  .catch((err) => {
-    console.log("OH NO MONGO CONNECTION ERROR!!!!");
-    console.log(err);
-  });
+mongoose.connect("mongodb://localhost:27017/yelp-camp", {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+  console.log("Database connected");
+});
 
 const sample = (array) => array[Math.floor(Math.random() * array.length)];
 
 const seedDB = async () => {
   await Campground.deleteMany({});
   for (let i = 0; i < 50; i++) {
-    const random1000 = Math.floor(Math.random() * 210);
+    const random1000 = Math.floor(Math.random() * 1000);
     const price = Math.floor(Math.random() * 20) + 10;
     const camp = new Campground({
-      author: "61ea9688c53326b013bded5f",
+      author: "5f5c330c2cd79d538f2c66d9",
       location: `${cities[random1000].city}, ${cities[random1000].state}`,
       title: `${sample(descriptors)} ${sample(places)}`,
-      image: "https://source.unsplash.com/random/483251",
       description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium voluptate natus voluptatem repellat quisquam? Rerum sequi at sint cum accusantium odio. Laudantium numquam doloribus mollitia corrupti vitae, aliquid ullam! Laudantium!",
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam dolores vero perferendis laudantium, consequuntur voluptatibus nulla architecto, sit soluta esse iure sed labore ipsam a cum nihil atque molestiae deserunt!",
       price,
+      images: [
+        {
+          url: "https://res.cloudinary.com/douqbebwk/image/upload/v1600060601/YelpCamp/ahfnenvca4tha00h2ubt.png",
+          filename: "YelpCamp/ahfnenvca4tha00h2ubt",
+        },
+        {
+          url: "https://res.cloudinary.com/douqbebwk/image/upload/v1600060601/YelpCamp/ruyoaxgf72nzpi4y6cdi.png",
+          filename: "YelpCamp/ruyoaxgf72nzpi4y6cdi",
+        },
+      ],
     });
     await camp.save();
   }
