a70d5c9ce2a10b5fc7ad6d6cf97450102876f7f5
samarjit-singh <75423063+samarjit-singh@users.noreply.github.com>
2022-02-01 22:21:05 +0530

    adding maps complete

--- public/javascripts/showPageMap.js
+++ public/javascripts/showPageMap.js
@@ -1,13 +1,3 @@
-// // const campground = require("../../models/campground");
-
-// mapboxgl.accessToken = mapToken;
-// const map = new mapboxgl.Map({
-//   container: "map", // container ID
-//   style: "mapbox://styles/mapbox/streets-v11", // style URL
-//   center: campground.geometry.coordinates, // starting position [lng, lat]
-//   zoom: 4, // starting zoom
-// });
-
 // new mapboxgl.Marker().setLngLat([-74.5, 40]).addTo(map);
 mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
--- seeds/index.js
+++ seeds/index.js
@@ -5,7 +5,7 @@ const Campground = require("../models/campground");
 
 mongoose.connect("mongodb://localhost:27017/yelp-camp", {
   useNewUrlParser: true,
-  useCreateIndex: true,
+  // useCreateIndex: true,
   useUnifiedTopology: true,
 });
 
@@ -21,15 +21,20 @@ const sample = (array) => array[Math.floor(Math.random() * array.length)];
 const seedDB = async () => {
   await Campground.deleteMany({});
   for (let i = 0; i < 50; i++) {
-    const random1000 = Math.floor(Math.random() * 1000);
+    const random1000 = Math.floor(Math.random() * 200);
     const price = Math.floor(Math.random() * 20) + 10;
     const camp = new Campground({
-      author: "5f5c330c2cd79d538f2c66d9",
+      //YOUR USER ID
+      author: "61ea9688c53326b013bded5f",
       location: `${cities[random1000].city}, ${cities[random1000].state}`,
       title: `${sample(descriptors)} ${sample(places)}`,
       description:
         "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam dolores vero perferendis laudantium, consequuntur voluptatibus nulla architecto, sit soluta esse iure sed labore ipsam a cum nihil atque molestiae deserunt!",
       price,
+      geometry: {
+        type: "Point",
+        coordinates: [-113.1331, 47.0202],
+      },
       images: [
         {
           url: "https://res.cloudinary.com/douqbebwk/image/upload/v1600060601/YelpCamp/ahfnenvca4tha00h2ubt.png",
